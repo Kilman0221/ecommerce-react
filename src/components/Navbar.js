b41412456b49a896d6303232114d6e5739383572
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2022-04-28 21:07:06 +0200

    added basic ul and li elements

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+export default function Navbar() {
+
+
+
+    return (
+        <nav>
+            <div className="logo">E-commerce</div>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <ul>
+                <li>Log-in</li>
+                <li>cart</li>
+            </ul>
+        </nav>
+    )
+}
