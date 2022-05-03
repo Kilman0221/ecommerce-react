8504cc2c3aa143cf606ca96bf1e4f5b581afd200
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2022-05-03 18:52:48 +0200

    created filter buttons

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,18 +6,19 @@ import Carousel from "./Carousel";
 export default function Products() {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
+    const [filter, setFilter] = useState()
     useEffect(() => {
 
         const getData = async () => {
             const res = await fetch('https://fakestoreapi.com/products');
 
-            setData(await res.json())
+            setData(await res.clone().json())
+            setFilter(await res.json())
 
             setLoading(false)
         }
 
         getData();
-
 
     }, [])
     console.log(data)
@@ -31,7 +32,13 @@ export default function Products() {
                 <Carousel />
             </div>
             <div className="filter">
-
+                <ul>
+                    <li><button>All</button></li>
+                    <li><button>Electronics</button></li>
+                    <li><button>Jewelery</button></li>
+                    <li><button>Men's clothing</button></li>
+                    <li><button>Women's clothing</button></li>
+                </ul>
             </div>
             <div className="products"></div>
         </section>
