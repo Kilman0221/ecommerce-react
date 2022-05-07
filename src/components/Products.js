ed012296bd54a69053dbeeb0b033ab168c1d6ec5
Kilman0221 <85839038+Kilman0221@users.noreply.github.com>
2022-05-07 15:35:56 +0200

    added rating to product card

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,7 +12,8 @@ export default function Products(props) {
             <div className="product-details">
                 <h2>{props.title}</h2>
                 <h3>Price: {props.price}$</h3>
-                <a href="#">Go to product</a>
+                <a href="#">See more</a>
+                <h3>Rating: {props.rating.rate}</h3>
             </div>
 
         </div>
