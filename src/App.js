d5760b121f2bd78ec2e4b64737692256a9974437
Kilman0221 <85839038+SzWoz@users.noreply.github.com>
2022-05-11 22:29:39 +0200

    started implementing react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 function App() {
