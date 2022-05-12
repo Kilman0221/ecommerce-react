d859ce5f2f31fa677e6a04a798407ee7c8e4265f
SzWoz <85839038+SzWoz@users.noreply.github.com>
2022-05-12 22:09:02 +0200

    color changes

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router'
 
 export default function ProductPage() {
 
-    const { prodId } = useParams();
+    const { id } = useParams();
     const [productDetails, setProductDetails] = useState();
 
     useEffect(() => {
         const fetchProduct = async () => {
-            const call = await (`fetch('https://fakestoreapi.com/products/${prodId}')`)
+            const call = await fetch(`https://fakestoreapi.com/products/${id}`)
             const res = await call.json();
 
             setProductDetails(res)
         }
         fetchProduct()
 
-        console.log(productDetails)
     }, [])
 
+    console.log(productDetails)
 
     return (
         <div className="product">
+
         </div>
     )
 }
